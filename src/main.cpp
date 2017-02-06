498a3eb92660d260a249d0ac550f297211764e9e
Matt Corallo <git@bluematt.me>
2017-02-06 17:38:57 -0500

    Make nTimeBestReceived atomic
    zcash: cherry picked from commit d8f2b8a8c032b83a3bd90750e58abaeece7e34e7
    zcash: https://github.com/bitcoin/bitcoin/pull/9708

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ void komodo_broadcast(const CBlock *pblock,int32_t limit);
 BlockMap mapBlockIndex;
 CChain chainActive;
 CBlockIndex *pindexBestHeader = NULL;
-static int64_t nTimeBestReceived = 0;
+static std::atomic<int64_t> nTimeBestReceived(0); // Used only to inform the wallet of when we last received a block
 CWaitableCriticalSection csBestBlock;
 CConditionVariable cvBlockChange;
 int nScriptCheckThreads = 0;
