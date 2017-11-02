847cd81b5aba952a17d0506ba56cb98261c17fcb
Cory Fields <cory-nospam-@coryfields.com>
2017-11-02 14:37:33 -0400

    rpc: further constrain the libevent workaround
    The bug was introduced in 2.1.6-beta, versions before that don't need the
    workaround.
    
    zcash: currently, we build with libevent-2.1.12, so this fix is needed
    zcash: cherry picked from commit 97932cd2689659addfbb58dc6148928b73af3bd0
    zcash: https://github.com/bitcoin/bitcoin/pull/11593

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -224,7 +224,7 @@ static std::string RequestMethodString(HTTPRequest::RequestMethod m)
 static void http_request_cb(struct evhttp_request* req, void* arg)
 {
     // Disable reading to work around a libevent bug, fixed in 2.2.0.
-    if (event_get_version_number() < 0x02020001) {
+    if (event_get_version_number() >= 0x02010600 && event_get_version_number() < 0x02020001) {
         evhttp_connection* conn = evhttp_request_get_connection(req);
         if (conn) {
             bufferevent* bev = evhttp_connection_get_bufferevent(conn);
@@ -607,10 +607,9 @@ void HTTPRequest::WriteReply(int nStatus, const std::string& strReply)
     evbuffer_add(evb, strReply.data(), strReply.size());
     auto req_copy = req;
     HTTPEvent* ev = new HTTPEvent(eventBase, true, [req_copy, nStatus]{
-        evhttp_send_reply(req_copy, nStatus, nullptr, nullptr);
         // Re-enable reading from the socket. This is the second part of the libevent
         // workaround above.
-        if (event_get_version_number() < 0x02020001) {
+        if (event_get_version_number() >= 0x02010600 && event_get_version_number() < 0x02020001) {
             evhttp_connection* conn = evhttp_request_get_connection(req_copy);
             if (conn) {
                 bufferevent* bev = evhttp_connection_get_bufferevent(conn);
@@ -619,6 +618,7 @@ void HTTPRequest::WriteReply(int nStatus, const std::string& strReply)
                 }
             }
         }
+        evhttp_send_reply(req_copy, nStatus, nullptr, nullptr);
     });
     ev->trigger(0);
     replySent = true;
