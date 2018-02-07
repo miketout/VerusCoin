325002d17183ddbf517cb1c4ac2d45c46d1caf6d
Wladimir J. van der Laan <laanwj@gmail.com>
2018-02-07 09:53:46 +0100

    http: Remove numThreads and ThreadCounter
    The HTTP worker thread counter, as well as the RAII object that was used
    to maintain it, is unused now, so can be removed.
    
    Signed-off-by: Wladimir J. van der Laan <laanwj@gmail.com>
    
    zcash: cherry picked from commit 11e01515fe0fbc7823d4111ad6e016a02c485a78
    zcash: https://github.com/bitcoin/bitcoin/pull/12366

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -74,30 +74,10 @@ private:
     std::deque<WorkItem*> queue;
     bool running;
     size_t maxDepth;
-    int numThreads;
-
-    /** RAII object to keep track of number of running worker threads */
-    class ThreadCounter
-    {
-    public:
-        WorkQueue &wq;
-        ThreadCounter(WorkQueue &w): wq(w)
-        {
-            boost::lock_guard<boost::mutex> lock(wq.cs);
-            wq.numThreads += 1;
-        }
-        ~ThreadCounter()
-        {
-            boost::lock_guard<boost::mutex> lock(wq.cs);
-            wq.numThreads -= 1;
-            wq.cond.notify_all();
-        }
-    };
 
 public:
     WorkQueue(size_t maxDepth) : running(true),
-                                 maxDepth(maxDepth),
-                                 numThreads(0)
+                                 maxDepth(maxDepth)
     {
     }
     /*( Precondition: worker threads have all stopped (they have been joined).
@@ -123,7 +103,6 @@ public:
     /** Thread function */
     void Run()
     {
-        ThreadCounter count(*this);
         while (running) {
             WorkItem* i = 0;
             {
