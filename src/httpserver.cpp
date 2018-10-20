b4efa96c1c0f71c43b090d398b3c92516d2891a2
Luke Dashjr <luke-jr+git@utopios.org>
2018-10-20 14:56:58 +0000

    rpcbind: Warn about exposing RPC to untrusted networks
    (cherry picked from commit 27c44ef9c61f64d941ab82ec232a68141a2fde90)

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -350,6 +350,11 @@ static bool HTTPBindAddresses(struct evhttp* http)
         LogPrint("http", "Binding RPC on address %s port %i\n", i->first, i->second);
         evhttp_bound_socket *bind_handle = evhttp_bind_socket_with_handle(http, i->first.empty() ? NULL : i->first.c_str(), i->second);
         if (bind_handle) {
+            std::vector<CNetAddr> vIP;
+            if (i->first.empty() ||
+                (LookupHost(i->first.c_str(), vIP, 1, false) && !vIP.empty() && vIP[0].IsBindAny())) {
+                LogPrintf("WARNING: the RPC server is not safe to expose to untrusted networks such as the public internet\n");
+            }
             boundSockets.push_back(bind_handle);
         } else {
             LogPrintf("Binding RPC on address %s port %i failed.\n", i->first, i->second);
