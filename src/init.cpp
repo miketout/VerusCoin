3baf91c2015ba600c3d9c24fd035c392699ee429
Daira Hopwood <daira@jacaranda.org>
2020-03-08 11:06:29 +0000

    Exit init early if we request shutdown before having loaded the genesis block.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/init.cpp
+++ src/init.cpp
@@ -2363,6 +2363,9 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
             MilliSleep(10);
         }
     }
+    if (!fHaveGenesis) {
+        return false;
+    }
 
     // ********************************************************* Step 11: start node
 
