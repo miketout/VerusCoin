67d02dabd09725307050b0176bfb54586bb035e8
Carl Dong <contact@carldong.me>
2020-08-08 13:02:00 -0700

    depends: boost: Refer to version in URL
    (cherry picked from commit 63390e503c16f40f802ba736bbc49b74423c8da2)

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,7 +1,7 @@
 package=boost
 $(package)_version=1_74_0
 $(package)_download_path=https://archives.boost.io/release/$(subst _,.,$($(package)_version))/source/
-$(package)_file_name=boost_$($(package)_version).tar.bz2
+$(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=83bfc1507731a0906e387fc28b7ef5417d591429e51e788417fe9ff025e116b1
 $(package)_patches=signals2-noise.patch ignore_wnonnull_gcc_11.patch range_enums_clang_16.patch unary_function.patch
 
