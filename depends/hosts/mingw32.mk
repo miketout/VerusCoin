1d1d0e8409dbdb60e50594a95a815f7f1f13fe2e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-05-28 09:28:14 +0300

    build: Try posix-specific CXX first for mingw32 host
    (cherry picked from commit f09ed92be132ebcb91b459c87d640a14b4b54336)

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -1,5 +1,8 @@
 mingw32_CC=x86_64-w64-mingw32-gcc-posix
-mingw32_CXX=x86_64-w64-mingw32-g++-posix
+ifneq ($(shell which $(host)-g++-posix),)
+mingw32_CXX := $(host)-g++-posix
+endif
+
 mingw32_CFLAGS=-pipe
 mingw32_CXXFLAGS=$(mingw32_CFLAGS)
 
