e96bdb39bba4585b55b222989b88b3d9f5319855
miketout <michaeltoutonghi@gmail.com>
2025-02-26 17:23:44 -0800

    Height fix

--- src/pbaas/reserves.cpp
+++ src/pbaas/reserves.cpp
@@ -5809,7 +5809,7 @@ bool CReserveTransactionDescriptor::AddReserveTransferImportOutputs(const CCurre
                 scratchCurrencyState.ConvertAmounts(adjustedReserveConverted.AsCurrencyVector(importCurrencyState.currencies),
                                                     fractionalConverted.AsCurrencyVector(importCurrencyState.currencies),
                                                     dummyCurState,
-                                                    ConnectedChains.IsPromoteExchangeRate(height) || (IsVerusActive() && importCurrencyState.GetID() == ConnectedChains.KaijuCurrencyID() && height >= 3558609),
+                                                    ConnectedChains.IsPromoteExchangeRate(height) || (IsVerusActive() && importCurrencyState.GetID() == ConnectedChains.KaijuCurrencyID() && height >= 3448609),
                                                     ConnectedChains.IsPBaaSRefundFixActive(height),
                                                     state,
                                                     &crossConversions,
