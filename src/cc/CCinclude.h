b2ab6b401e89865e5ce06ec3ddd0b21a6c2d3c6f
Alright <mzlot555@gmail.com>
2025-10-24 16:09:23 -0400

    remove dead code CCtx.cpp function AddNormalinputs

--- src/cc/CCinclude.h
+++ src/cc/CCinclude.h
@@ -165,7 +165,6 @@ int64_t CCduration(int32_t &numblocks,uint256 txid);
 bool SignTx(CMutableTransaction &mtx,int32_t vini,int64_t utxovalue,const CScript scriptPubKey);
 void SetCCunspents(std::vector<std::pair<CAddressUnspentKey, CAddressUnspentValue> > &unspentOutputs,char *coinaddr);
 void SetCCtxids(std::vector<std::pair<CAddressIndexKey, CAmount> > &addressIndex,char *coinaddr);
-int64_t AddNormalinputs(CMutableTransaction &mtx,CPubKey mypk,int64_t total,int32_t maxinputs);
 int64_t CCutxovalue(char *coinaddr,uint256 utxotxid,int32_t utxovout);
 
 // curve25519 and sha256
