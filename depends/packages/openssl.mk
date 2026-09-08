1de081362dc49cb412ccf88fb2aebcdc28217e51
Asher Dawes <asher.dawes@gmail.com>
2026-09-08 12:06:05 -0700

    Point the depends OpenSSL default certificate directory at /etc/ssl

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash=cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d9392520
 
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
-$(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
+$(package)_config_opts=--prefix=$(host_prefix) --openssldir=/etc/ssl
 $(package)_config_opts+=no-afalgeng
 $(package)_config_opts+=no-asm
 $(package)_config_opts+=no-async
